e62628035eb6c5bbd4632ffdc7a1406e32cb84d4
JosephDotGit <112686870+JosephDotGit@users.noreply.github.com>
2024-07-29 22:28:06 +0700

    Update app.py

--- Assignment/Week-2/Backend/app.py
+++ Assignment/Week-2/Backend/app.py
@@ -28,7 +28,7 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://localhost:5173",  # frontend domain
-    "https://iot-kmitl-week-2-backend.vercel.app/api/v1",
+    "https://iot-kmitl-week-2-backend.vercel.app",
     "https://iot-kmitl-week-2.vercel.app"
 ]
 
